3529c7b75c241ae98a4ebec74665c63571718471
Yannis <yannisfj@gmail.com>
2019-06-09 21:41:42 +0000

    js border functions

--- js/main.js
+++ js/main.js
@@ -4,10 +4,14 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 //select tab content item
 
 function selectItem(e) {
+    removeBorder();
     //add border to current tab
     this.classList.add('tab-border');
 }
 
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
 
 //listen for tab click
 tabItems.forEach(item => item.addEventListener('click', selectItem));
